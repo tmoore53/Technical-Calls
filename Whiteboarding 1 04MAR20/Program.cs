64231b20cdf81ecdd9d183056f823817b60ffd0d
Tyler Moore <tyler.moore53@outlook.com>
2020-03-12 07:33:08 -0700

    Commit to Git

---
+++ Whiteboarding 1 04MAR20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whiteboarding_1_04MAR20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarr = {  1, 1, 5, 2, 5, 2, 3, 3, 6, 4, 6, 4};
+            Console.WriteLine();
+            Console.WriteLine(RepByteArr(myarr));
+        }
+        //take a byte array with values from 0 - 255
+        //
+        public static bool RepByteArr(int[] x)
+        {
+            Array.Sort(x);
+            for(int i = 0; i < x.Length/2; i =+ 2)
+            {
+                if (x[i] == x[i+1] )
+                {
+                    return true;
+                }
+                break;
+            }
+            return false;
+
+        }
+
+    }
+    
+  
+}
